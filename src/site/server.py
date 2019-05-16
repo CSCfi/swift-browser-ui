52c2b5b677d3b0bdb2a7c6884de94452f84e1621
sampsa.penna <sampsa.penna@csc.fi>
2019-05-16 14:27:11 +0300

    added session middleware and refactored the app initialization into a function

--- src/site/server.py
+++ src/site/server.py
@@ -6,36 +6,49 @@ import aiohttp.web
 import os
 import ssl
 
+from aiohttp_session import setup as session_setup
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+import cryptography.fernet
+
 # Project specific imports
 from route import routel
 
 
-PROJECT_ROOT = os.getcwd()
+def servinit():
+    PROJECT_ROOT = os.getcwd()
 
+    app = aiohttp.web.Application()
 
-app = aiohttp.web.Application()
+    session_setup(
+        app,
+        EncryptedCookieStorage(
+            cryptography.fernet.Fernet.generate_key().decode('utf-8')
+        )
+    )
 
+    # Setup static folder during developement
+    app.router.add_static(
+        '/static/',
+        path=PROJECT_ROOT + '/static',
+        name='static',
+        show_index=True,
+    )
 
-# Setup static folder during developement
-app.router.add_static(
-    '/static/',
-    path=PROJECT_ROOT + '/static',
-    name='static',
-    show_index=True,
-)
+    # Setup all routes from API and frontend modules
+    app.add_routes(
+        routel
+    )
 
-# Setup all routes from API and frontend modules
-app.add_routes(
-    routel
-)
+    # Setup ssl context (FUTURE)
+    # sslcontext = ssl.create_default_context()
+    # sslcontext.set_ciphers(
+    #     'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-'+
+    #     'CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-' +
+    #     'SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA't+
+    #     '-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256'
+    # )
 
-# Setup ssl context (FUTURE)
-# sslcontext = ssl.create_default_context()
-# sslcontext.set_ciphers(
-#     'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-'+
-#     'CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-' +
-#     'SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA't+
-#     '-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256'
-# )
+    return app
 
-aiohttp.web.run_app(app)
+if __name__ == '__main__':
+    aiohttp.web.run_app(servinit())
