50472eeb9a91f093fbd5334b867da93530c98c7c
sampsa.penna <sampsa.penna@csc.fi>
2019-05-20 10:59:51 +0300

    Added support for client sessions to store information and prevent unauthenticated access to both the API and the frontend.

--- src/site/api.py
+++ src/site/api.py
@@ -6,14 +6,10 @@ import boto3
 import datetime
 
 
-# TODO: Implement dynamic credential passing
 # TODO: Check if it's necessary to change the API from s3 -> swift
 # Hardcoded s3 access keys for testing purposes, will be redundant when
 # authentication is implemented (hopefully at least)
 ***REMOVED******REMOVED***AWS_ENDPOINT_URL = "http://127.0.0.1:9000"
-
-
-API_ENDPOINT = '/api'
 
 
 async def list_buckets(request):
@@ -21,16 +17,23 @@ async def list_buckets(request):
     The internal API call for fetching a list of buckets available for user
     """
     # TODO: Perhaps think up a way to keep the s3 sessions persistent?
-    s3 = boto3.client(
-        's3',
-***REMOVED******REMOVED******REMOVED***    )
-    ret = s3.list_buckets()['Buckets']
-    for i in ret:
-        i['CreationDate'] = i['CreationDate'].ctime()
+    try:
+        session = request.cookies['S3BROW_SESSION']
+        s3 = boto3.client(
+            's3',
+***REMOVED******REMOVED******REMOVED***        )
+        ret = s3.list_buckets()['Buckets']
+        for i in ret:
+            i['CreationDate'] = i['CreationDate'].ctime()
 
-    return aiohttp.web.json_response(
-        ret
-    )
+        return aiohttp.web.json_response(
+            ret
+        )
+    except KeyError:
+        return aiohttp.web.Response(
+            status=403,
+            reason="No user session was found"
+        )
 
 
 async def list_objects(request):
@@ -38,18 +41,25 @@ async def list_objects(request):
     The internal API call for fetching a list of available objects inside
     a specified bucket
     """
-    s3 = boto3.client(
-        's3',
-***REMOVED******REMOVED******REMOVED***    )
-    ret = s3.list_objects(
-        Bucket=request.query['bucket']
-    )['Contents']
-    for i in ret:
-        i['LastModified'] = i['LastModified'].ctime()
+    try:
+        session = request.cookies['S3BROW_SESSION']
+        s3 = boto3.client(
+            's3',
+***REMOVED******REMOVED******REMOVED***        )
+        ret = s3.list_objects(
+            Bucket=request.query['bucket']
+        )['Contents']
+        for i in ret:
+            i['LastModified'] = i['LastModified'].ctime()
 
-    return aiohttp.web.json_response(
-        ret
-    )
+        return aiohttp.web.json_response(
+            ret
+        )
+    except KeyError:
+        return aiohttp.web.Response(
+            status=403,
+            reason="No user session was found"
+        )
 
 
 async def download_object(dloadrequest):
@@ -57,23 +67,30 @@ async def download_object(dloadrequest):
     The internal API call for mapping an object to a websocket, to make enable
     object streaming.
     """
-    s3 = boto3.client(
-        's3',
-***REMOVED******REMOVED******REMOVED***    )
+    try:
+        session = dloadrequest.cookies['S3BROW_SESSION']
+        s3 = boto3.client(
+            's3',
+***REMOVED******REMOVED******REMOVED***        )
 
-    url = s3.generate_presigned_url(
-        'get_object',
-        Params={
-            'Bucket': dloadrequest.query['bucket'],
-            'Key': dloadrequest.query['objkey']
-        },
-        ExpiresIn=600
-    )
+        url = s3.generate_presigned_url(
+            'get_object',
+            Params={
+                'Bucket': dloadrequest.query['bucket'],
+                'Key': dloadrequest.query['objkey']
+            },
+            ExpiresIn=1
+        )
 
-    response = aiohttp.web.Response()
-    response.set_status(303)
-    response.headers.add(
-        'Location', url
-    )
+        response = aiohttp.web.Response()
+        response.set_status(303)
+        response.headers.add(
+            'Location', url
+        )
 
-    return response
+        return response
+    except KeyError:
+        return aiohttp.web.Response(
+            status=403,
+            reason="No user session was found"
+        )
