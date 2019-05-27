44c46311942ad5eb8440f234e926119ca6f8db27
sampsa.penna <sampsa.penna@csc.fi>
2019-05-27 10:47:08 +0300

    Refactored init file

--- s3browser/__init__.py
+++ s3browser/__init__.py
@@ -1,12 +1,11 @@
-from .server import servinit, run_server_secure
-import s3browser.api
-import s3browser.login
-import s3browser.front
-
 """
 Web application for browsing s3 compliant object storage, with openstack-\
 keystone federated authentication integrated. Frontend with static files.
 """
+
+
+import s3browser.server
+
 
 __name__ = 's3browser_backend'
 __version__ = VERSION = '0.0.1rc1'
