7ee198e77d97f66eb5d153b85d179612b481f02e
sampsa.penna <sampsa.penna@csc.fi>
2019-05-29 17:32:31 +0300

    Removed imports from init for now, since the required default modules are not yet fully known

--- s3browser/__init__.py
+++ s3browser/__init__.py
@@ -4,9 +4,6 @@ keystone federated authentication integrated. Frontend with static files.
 """
 
 
-import s3browser.server
-
-
 __name__ = 's3browser_backend'
 __version__ = VERSION = '0.0.1rc1'
 __author__ = 'Sampsa Penna'
