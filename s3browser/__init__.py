6dfba180d308e7fbb8024654a1a70fd73b2d76d0
sampsa.penna <sampsa.penna@csc.fi>
2019-05-31 10:39:54 +0300

    Changed package name to the correct one

--- s3browser/__init__.py
+++ s3browser/__init__.py
@@ -4,7 +4,7 @@ keystone federated authentication integrated. Frontend with static files.
 """
 
 
-__name__ = 's3browser_backend'
+__name__ = 's3browser'
 __version__ = VERSION = '0.0.1rc1'
 __author__ = 'Sampsa Penna'
 __license__ = 'MIT License'
