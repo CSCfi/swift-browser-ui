33df938fbe11672e4ac9e334a5c3af591230b0a4
sampsa.penna <sampsa.penna@csc.fi>
2019-06-06 12:49:42 +0300

    Added a file for mock classes

---
+++ tests/mockups.py
@@ -0,0 +1,45 @@
+"""
+This module contains the mock-up classes and functions, used for testing the
+s3browser package.
+"""
+
+
+class Mock_Request:
+    """
+    Mock-up class for the aiohttp.web.Request, which contains the dictionary
+    representation of the requests that will be passed to the functions. (the
+    actual request eing a MutableMapping instance)
+    """
+    def __init__(self):
+        pass
+
+
+class Mock_App:
+    """
+    Mock-up class for the aiohttp.web.Application, which contains the
+    dictionary representation of the Application. (the actual application
+    being a MutableMapping instance)
+    """
+    def __init__(self):
+        pass
+
+
+class Mock_Service:
+    """
+    Mock-up class for the Openstack service, in this case a swiftclient.Service
+    instance. Contains the mock-ups for the relevant methods used in this
+    project. Also contains functions for generating test data, in case it is
+    necessary.
+    """
+    def __init__(self):
+        pass
+
+
+class Mock_Session:
+    """
+    Mock--up class for the Openstack keystoneauth1 session instance, which
+    contains the relevant methods for querying the OS identity API (aka.
+    keystone)
+    """
+    def __init__(self):
+        pass
