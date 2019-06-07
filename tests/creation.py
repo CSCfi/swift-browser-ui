e693afc8df83f60f995018f1f9ac1980cbc5cb0e
sampsa.penna <sampsa.penna@csc.fi>
2019-06-07 13:53:30 +0300

    Added logger for the mockup request

--- tests/creation.py
+++ tests/creation.py
@@ -5,7 +5,9 @@ of the test environment.
 
 
 import cryptography.fernet
-from mockups import Mock_Request
+from mockups import Mock_Request, Mock_Service, Mock_Session
+from s3browser._convenience import generate_cookie
+import logging
 
 
 def get_request_with_fernet():
@@ -15,7 +17,22 @@ def get_request_with_fernet():
     ret = Mock_Request()
     ret.app['Sessions'] = []
     ret.app['Creds'] = {}
+    ret.app['Log'] = logging.getLogger(name="test_logger")
     ret.app['Crypt'] = cryptography.fernet.Fernet(
         cryptography.fernet.Fernet.generate_key()
     )
     return ret
+
+
+def get_request_with_mock_openstack():
+    """
+    Create a request with a "working" openstack mock-up service and session
+    """
+    ret = get_request_with_fernet()
+    cookie, ret.cookies['S3BROW_SESSION'] = generate_cookie(ret)
+    ret.app['Sessions'].append(cookie)
+    ret.app['Creds'][cookie] = {}
+    ret.app['Creds'][cookie]['OS_sess'] = Mock_Session()
+    ret.app['Creds'][cookie]['ST_conn'] = Mock_Service()
+    ret.app['Creds'][cookie]['Avail'] = "Placeholder"
+    return cookie, ret
