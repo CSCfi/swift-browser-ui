d54c2d000b09f5b08034b85f04883d0555ad34d8
sampsa.penna <sampsa.penna@csc.fi>
2019-06-07 15:45:17 +0300

    Added test function for s3browser.server.servinit.

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,3 +1,18 @@
 """
 Module for testing s3browser.server
 """
+
+
+import pytest
+from s3browser.server import servinit
+
+
+@pytest.mark.asyncio
+async def test_servinit():
+    """
+    Function for testing server initialization function execution
+    """
+    # Don't really need much testing here, if the server initialization
+    # executes to the end all is fine.
+    app = servinit()
+    assert app is not None  # nosec
