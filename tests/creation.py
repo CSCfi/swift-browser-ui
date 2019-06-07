95eaac3de6f65552195bfe7a16a680962c19f7ab
sampsa.penna <sampsa.penna@csc.fi>
2019-06-07 10:02:33 +0300

    Added function to initialize mock application with a Fernet object

--- tests/creation.py
+++ tests/creation.py
@@ -2,3 +2,20 @@
 This module contains some frequently used constructors to ease in the building
 of the test environment.
 """
+
+
+import cryptography.fernet
+from mockups import Mock_Request
+
+
+def get_request_with_fernet():
+    """
+    Create a request with a working fernet object
+    """
+    ret = Mock_Request()
+    ret.app['Sessions'] = []
+    ret.app['Creds'] = []
+    ret.app['Crypt'] = cryptography.fernet.Fernet(
+        cryptography.fernet.Fernet.generate_key()
+    )
+    return ret
