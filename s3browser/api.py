ec300282add4fd56c37354fc1d0bab1cce3b02a0
sampsa.penna <sampsa.penna@csc.fi>
2019-06-17 11:41:55 +0300

    Comment out the old s3 functions to prepare for a future removal of code

--- s3browser/api.py
+++ s3browser/api.py
@@ -1,115 +1,115 @@
 import aiohttp.web
-import boto3
+# import boto3
 import time
 from swiftclient.service import SwiftError
 
-from ._convenience import decrypt_cookie
+# from ._convenience import decrypt_cookie
 from ._convenience import api_check
 
 
 # Toggle S3 function overloading
-SETUP_WITH_S3 = False
+# SETUP_WITH_S3 = False
 
 
-async def s3_list_buckets(request):
-    """
-    The internal API call for fetching a list of buckets available for user
-    """
-    # TODO: Refactor code to store session specific s3 client to app mapping
-    try:
-        if decrypt_cookie(request) not in request.app['Sessions']:
-            raise KeyError()
-
-        s3 = boto3.client(
-            's3',
-        )
-        # Fetch the information about the user's buckets from API
-        ret = s3.list_buckets()['Buckets']
-        for i in ret:
-            # Transform the CreationDate into a user readable string, since
-            # the datetime-object is not JSON-serializeable
-            i['CreationDate'] = i['CreationDate'].ctime()
-
-        return aiohttp.web.json_response(
-            ret
-        )
-    # If can't find user session, reply with 401
-    except KeyError:
-        return aiohttp.web.Response(
-            status=401,
-            reason="No user session was found"
-        )
-
-
-async def s3_list_objects(request):
-    """
-    The internal API call for fetching a list of available objects inside
-    a specified bucket
-    """
-    try:
-        if decrypt_cookie(request) not in request.app['Sessions']:
-            raise KeyError()
-        s3 = boto3.client(
-            's3',
-        )
-        # Get all objects in the specified bucket
-        ret = s3.list_objects(
-            Bucket=request.query['bucket']
-        )['Contents']
-        for i in ret:
-            # Transform the LastModified date value into a user readable
-            # string, since the datetime-object is not JSON-serializeable
-            i['LastModified'] = i['LastModified'].ctime()
-
-        return aiohttp.web.json_response(
-            ret
-        )
-    except KeyError:
-        # If can't find user session, reply with 401
-        return aiohttp.web.Response(
-            status=401,
-            reason="No user session was found"
-        )
+# async def s3_list_buckets(request):
+#     """
+#     The internal API call for fetching a list of buckets available for user
+#     """
+#     # TODO: Refactor code to store session specific s3 client to app mapping
+#     try:
+#         if decrypt_cookie(request) not in request.app['Sessions']:
+#             raise KeyError()
+#
+#         s3 = boto3.client(
+#             's3',
+#         )
+#         # Fetch the information about the user's buckets from API
+#         ret = s3.list_buckets()['Buckets']
+#         for i in ret:
+#             # Transform the CreationDate into a user readable string, since
+#             # the datetime-object is not JSON-serializeable
+#             i['CreationDate'] = i['CreationDate'].ctime()
+#
+#         return aiohttp.web.json_response(
+#             ret
+#         )
+#     # If can't find user session, reply with 401
+#     except KeyError:
+#         return aiohttp.web.Response(
+#             status=401,
+#             reason="No user session was found"
+#         )
 
 
-async def s3_download_object(dloadrequest):
-    """
-    Function to pull a short-lived presigned download URL from the s3 server
-    """
-    # TODO: implement exception handling and debug messages for URL fetching
-    try:
-        # Check for established session
-        # TODO: change over to API specific cookie
-        if (decrypt_cookie(dloadrequest) not in
-                dloadrequest.app['Sessions']):
-            raise KeyError()
-        # Open a client to the server
-        s3 = boto3.client(
-            's3',
-        )
-        # Get a presigned url from the server with a 2000ms TTL
-        url = s3.generate_presigned_url(
-            'get_object',
-            Params={
-                'Bucket': dloadrequest.query['bucket'],
-                'Key': dloadrequest.query['objkey']
-            },
-            ExpiresIn=2
-        )
-        # Re-direct the user to the presigned URL
-        response = aiohttp.web.Response()
-        response.set_status(303)
-        response.headers.add(
-            'Location', url
-        )
+# async def s3_list_objects(request):
+#     """
+#     The internal API call for fetching a list of available objects inside
+#     a specified bucket
+#     """
+#     try:
+#         if decrypt_cookie(request) not in request.app['Sessions']:
+#             raise KeyError()
+#         s3 = boto3.client(
+#             's3',
+#         )
+#         # Get all objects in the specified bucket
+#         ret = s3.list_objects(
+#             Bucket=request.query['bucket']
+#         )['Contents']
+#         for i in ret:
+#             # Transform the LastModified date value into a user readable
+#             # string, since the datetime-object is not JSON-serializeable
+#             i['LastModified'] = i['LastModified'].ctime()
+#
+#         return aiohttp.web.json_response(
+#             ret
+#         )
+#     except KeyError:
+#         # If can't find user session, reply with 401
+#         return aiohttp.web.Response(
+#             status=401,
+#             reason="No user session was found"
+#         )
 
-        return response
-    # If can't find user session, reply with 401
-    except KeyError:
-        return aiohttp.web.Response(
-            status=401,
-            reason="No user session was found"
-        )
+
+# async def s3_download_object(dloadrequest):
+#     """
+#     Function to pull a short-lived presigned download URL from the s3 server
+#     """
+#     # TODO: implement exception handling and debug messages for URL fetching
+#     try:
+#         # Check for established session
+#         # TODO: change over to API specific cookie
+#         if (decrypt_cookie(dloadrequest) not in
+#                 dloadrequest.app['Sessions']):
+#             raise KeyError()
+#         # Open a client to the server
+#         s3 = boto3.client(
+#             's3',
+#         )
+#         # Get a presigned url from the server with a 2000ms TTL
+#         url = s3.generate_presigned_url(
+#             'get_object',
+#             Params={
+#                 'Bucket': dloadrequest.query['bucket'],
+#                 'Key': dloadrequest.query['objkey']
+#             },
+#             ExpiresIn=2
+#         )
+#         # Re-direct the user to the presigned URL
+#         response = aiohttp.web.Response()
+#         response.set_status(303)
+#         response.headers.add(
+#             'Location', url
+#         )
+#
+#         return response
+#     # If can't find user session, reply with 401
+#     except KeyError:
+#         return aiohttp.web.Response(
+#             status=401,
+#             reason="No user session was found"
+#         )
 
 
 async def get_os_user(request):
@@ -237,11 +237,11 @@ async def os_list_projects(request):
 
 # Re-map functions that are actually used in the program, depending on which
 # platform to use – s3 or swift
-if SETUP_WITH_S3:
-    list_buckets = s3_list_buckets
-    list_objects = s3_list_objects
-    download_object = s3_download_object
-else:
-    list_buckets = swift_list_buckets
-    list_objects = swift_list_objects
-    download_object = swift_download_object
+# if SETUP_WITH_S3:
+#     list_buckets = s3_list_buckets
+#     list_objects = s3_list_objects
+#     download_object = s3_download_object
+# else:
+list_buckets = swift_list_buckets
+list_objects = swift_list_objects
+download_object = swift_download_object
