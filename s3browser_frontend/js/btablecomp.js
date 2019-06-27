5acec4d40248091a826c69eb57fc66a682bb35ca
sampsa.penna <sampsa.penna@csc.fi>
2019-06-27 10:19:03 +0300

    Components for breadcrumbs

--- s3browser_frontend/js/btablecomp.js
+++ s3browser_frontend/js/btablecomp.js
@@ -97,3 +97,14 @@ Vue.component('user-list-element', {
 </li>\
     ',
 })
+
+// A vue.js component for the program breadcrumb
+Vue.component('breadcrumb-list-element', {
+    props: [ 'address', 'alias' ],
+    template: '\
+<li class=breadcrumb-element>\
+    > <router-link class="breadcrumb-link" v-bind:to="address">\
+    {{ alias }}</router-link>\
+</li>\
+    ',
+})
