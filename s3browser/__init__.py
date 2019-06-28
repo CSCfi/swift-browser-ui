cd2f16629b938fe6534c9cbedcd62bb30f62dae5
sampsa.penna <sampsa.penna@csc.fi>
2019-06-28 10:01:43 +0300

    Remove unnecessary version variable

--- s3browser/__init__.py
+++ s3browser/__init__.py
@@ -5,6 +5,6 @@ keystone federated authentication integrated. Frontend with static files.
 
 
 __name__ = 's3browser'
-__version__ = VERSION = '0.0.1rc1'
+__version__ = '0.0.1rc1'
 __author__ = 'Sampsa Penna'
 __license__ = 'MIT License'
