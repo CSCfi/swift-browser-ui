ed3eb3c4dd7231e3de5ee6cf59c4fb8841605fa2
sampsa.penna <sampsa.penna@csc.fi>
2019-07-12 09:40:40 +0300

    Update version to

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = f'{current_year}, CSC Developers'
 author = 'CSC Developers'
 
 # The full version, including alpha/beta/rc tags
-version = release = '0.1.0-beta'
+version = release = '0.2.0beta1'
 
 
 # -- General configuration ---------------------------------------------------
