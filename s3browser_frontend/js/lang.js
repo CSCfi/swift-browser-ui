b1445da2eb5cfae4a860bf149fac166258200bf4
Stefan Negru <stefan@blankdots.com>
2019-07-15 16:03:31 +0300

    add language messages for en and partly fi

---
+++ s3browser_frontend/js/lang.js
@@ -0,0 +1,61 @@
+// Ready translated locale messages
+const langPlaceholders = {
+    en: {
+    message: {
+        currentProj: 'Current project',
+        logOut: 'Log Out',
+        cscOrg: 'CSC - IT Center For Science LTD.',
+        devel: 'developed by',
+        table: {
+            name: 'Name',
+            objects: 'Objects',
+            size: 'Size',
+            modified: 'Last Modified',
+            paginated: 'Paginated',
+            pageNb: 'per page',
+            fileHash: 'Hash',
+            fileType: 'Type',
+            fileDown: 'File Download',
+        },
+        largeFileMessage: '',
+        download: 'Download',
+        downloadAlt: 'Donwload link for',
+        downloadAltLarge: 'Confirm download large file',
+        largeDownMessage: 'No large (> 1GiB) downloads enabled. \
+                           Click to enable them for the duration of the session.',
+        largeDownAction: 'Enable',
+        emptyContainer: 'This container is empty.',
+        emptyProject: 'The project doesn\'t contain any containers.',
+        }
+    },
+    fi: {
+        message: {
+            currentProj: 'Nykyinen projekti',
+            logOut: 'Log Out',
+            cscOrg: 'CSC – Tieteen Tietotekniikan Keskus Oy',
+            devel: 'developed by', // Needs Translation
+            table: {
+                name: 'Nimi',
+                // All below need Translation
+                objects: 'Objects',
+                size: 'Size',
+                modified: 'Last Modified',
+                paginated: 'Paginated',
+                pageNb: 'per page',
+                fileHash: 'Hash',
+                fileType: 'Type',
+                fileDown: 'File Download',
+            },
+            largeFileMessage: '',
+            download: 'Download',
+            downloadAlt: 'Donwload link for',
+            downloadAltLarge: 'Confirm download large file',
+            largeDownMessage: 'No large (> 1GiB) downloads enabled. \
+                               Click to enable them for the duration of the session.',
+            largeDownAction: 'Enable',
+            emptyContainer: 'This container is empty.',
+            emptyProject: 'The project doesn\'t contain any containers.',
+        }
+    }
+}
+
