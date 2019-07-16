1afb9ec7fd5d462ea86b61191145e28cf1b33473
sampsa.penna <sampsa.penna@csc.fi>
2019-07-16 10:00:14 +0300

    Add rudimentary Finnish translation

--- s3browser_frontend/js/lang.js
+++ s3browser_frontend/js/lang.js
@@ -19,7 +19,7 @@ const langPlaceholders = {
         },
         largeFileMessage: '',
         download: 'Download',
-        downloadAlt: 'Donwload link for',
+        downloadAlt: 'Download link for',
         downloadAltLarge: 'Confirm download large file',
         largeDownMessage: 'No large (> 1GiB) downloads enabled. \
                            Click to enable them for the duration of the session.',
@@ -32,31 +32,31 @@ const langPlaceholders = {
     fi: {
         message: {
             currentProj: 'Nykyinen projekti',
-            logOut: 'Log Out',
+            logOut: 'Kirjaudu ulos',
             cscOrg: 'CSC – Tieteen Tietotekniikan Keskus Oy',
-            devel: 'developed by', // Needs Translation
+            devel: 'kehittänyt',
             table: {
                 name: 'Nimi',
-                // All below need Translation
-                objects: 'Objects',
-                size: 'Size',
-                modified: 'Last Modified',
-                paginated: 'Paginated',
-                pageNb: 'per page',
-                fileHash: 'Hash',
-                fileType: 'Type',
-                fileDown: 'File Download',
+                objects: 'Objekteja',
+                size: 'Koko',
+                modified: 'Muokattu viimeksi',
+                paginated: 'Sivutus',
+                pageNb: 'sivulla',
+                fileHash: 'Tarkistussumma',
+                fileType: 'Tyyppi',
+                fileDown: 'Tiedoston lataus',
             },
             largeFileMessage: '',
-            download: 'Download',
-            downloadAlt: 'Donwload link for',
-            downloadAltLarge: 'Confirm download large file',
-            largeDownMessage: 'No large (> 1GiB) downloads enabled. \
-                               Click to enable them for the duration of the session.',
-            largeDownAction: 'Enable',
-            emptyContainer: 'This container is empty.',
-            emptyProject: 'The project doesn\'t contain any containers.',
-            searchBy: 'Search by Name',
+            download: 'Lataa',
+            downloadAlt: 'Latauslinkki tiedostolle',
+            downloadAltLarge: 'Hyväksy suuren tiedoston lataus',
+            largeDownMessage: 'Suurten tiedostojen (> 1Gt) lataus täytyy hyväksyä \
+                               erikseen. Paina hyväksyäksesi suuret lataukset nykyisen \
+                               kirjautumisen ajaksi.',
+            largeDownAction: 'Hyväksy',
+            emptyContainer: 'Säiliö on tyhjä.',
+            emptyProject: 'Projektilla ei ole säiliöitä.',
+            searchBy: 'Etsi nimellä',
         }
     }
 }
