99a1ea8ef8f6ca6e7e721c502d57527eec089575
sampsapenna <sampsa.penna@csc.fi>
2019-10-01 16:40:36 +0300

    Finish api function unit tests.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,12 @@
 """Test sharing backend API functions."""
 
 
+from types import SimpleNamespace
+
+
+import unittest.mock
 import asynctest
+import aiohttp.web
 
 
 from swift_x_account_sharing.api import (
@@ -14,71 +19,69 @@ from swift_x_account_sharing.api import (
 )
 
 
+from swift_x_account_sharing.dict_db import InMemDB
+
+
 class APITestClass(asynctest.TestCase):
     """Test the sharing backend public API."""
 
+    def setUp(self):
+        """Set up necessary mocks."""
+        self.mock_request = SimpleNamespace(**{
+            "app": {
+                "db_conn":
+                    unittest.mock.MagicMock(InMemDB)()
+            },
+            "query": {
+                "user": "AUTH_example",
+                "owner": "AUTH_otherexample",
+                "container": "test-container-1",
+                "access": "r,w,l",
+                "address": "https://placeholder.os:443"
+            },
+        })
+
+        self.json_mock = unittest.mock.MagicMock(
+            aiohttp.web.json_response
+        )
+
+        self.patch_json_dump = unittest.mock.patch(
+            "swift_x_account_sharing.api.aiohttp.web.json_response",
+            new=self.json_mock
+        )
+
     async def test_endpoint_has_access_correct(self):
         """Test the has-access endpoint for conformity."""
-        resp = await has_access_handler(None)
-        self.assertEqual(resp.status, 200)
+        with self.patch_json_dump:
+            await has_access_handler(self.mock_request)
+            self.json_mock.assert_called_once()
 
     async def test_endpoint_access_details_correct(self):
         """Test the access-details endpoint for conformity."""
-        resp = await access_details_handler(None)
-        self.assertEqual(resp.status, 200)
+        with self.patch_json_dump:
+            await access_details_handler(self.mock_request)
+            self.json_mock.assert_called_once()
 
     async def test_endpoint_gave_access_correct(self):
         """Test the gave-access endpoint for conformity."""
-        resp = await gave_access_handler(None)
-        self.assertEqual(resp.status, 200)
+        with self.patch_json_dump:
+            await gave_access_handler(self.mock_request)
+            self.json_mock.assert_called_once()
 
     async def test_endpoint_shared_details_correct(self):
         """Test the shared_details endpoint for conformity."""
-        resp = await shared_details_handler(None)
-        self.assertEqual(resp.status, 200)
+        with self.patch_json_dump:
+            await shared_details_handler(self.mock_request)
+            self.json_mock.assert_called_once()
 
     async def test_endpoint_share_container_correct(self):
         """Test the share_container endpoint for conformity."""
-        resp = await share_container_handler(None)
-        self.assertEqual(resp.status, 200)
+        with self.patch_json_dump:
+            resp = await share_container_handler(self.mock_request)
+            self.assertEqual(resp.status, 204)
 
     async def test_endpoint_unshare_container_correct(self):
         """Test the unshare_container endpoint for conformmity."""
-        resp = await unshare_container_handler(None)
-        self.assertEqual(resp.status, 200)
-
-    async def test_endpoint_has_access_noauth(self):
-        """Test the has-access endpoint without authorization."""
-
-    async def test_endpoint_access_details_noauth(self):
-        """Test the access-details endpoint without authorization."""
-
-    async def test_endpoint_gave_access_noauth(self):
-        """Test the gave-access endpoint without authorization."""
-
-    async def test_endpoint_shared_details_noauth(self):
-        """Test the shared_details endpoint without authorization."""
-
-    async def test_endpoint_share_container_noauth(self):
-        """Test the share_container endpoint without authorization."""
-
-    async def test_endpoint_unshare_container_noauth(self):
-        """Test the unshare_container endpoint without authorization."""
-
-    async def test_endpoint_has_access_missing(self):
-        """Test the has-access endpoint with missing information."""
-
-    async def test_endpoint_access_details_missing(self):
-        """Test the access-details endpoint with missing information."""
-
-    async def test_endpoint_gave_access_missing(self):
-        """Test the gave-access endpoint with missing information."""
-
-    async def test_endpoint_shared_details_missing(self):
-        """Test the shared_details endpoint with missing information."""
-
-    async def test_endpoint_share_container_missing(self):
-        """Test the share_container endpoint with missing information."""
-
-    async def test_endpoint_unshare_container_missing(self):
-        """Test the unshare_container endpoint with missing information."""
+        with self.patch_json_dump:
+            resp = await unshare_container_handler(self.mock_request)
+            self.assertEqual(resp.status, 204)
