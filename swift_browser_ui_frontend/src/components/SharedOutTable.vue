4a4918edc93917f25ae9575c75f5bf0ab8fd1e94
--global <sampsa.penna@csc.fi>
2019-10-08 17:09:40 +0300

    Add listing and details for the project's shared containers.

--- swift_browser_ui_frontend/src/components/SharedOutTable.vue
+++ swift_browser_ui_frontend/src/components/SharedOutTable.vue
@@ -27,19 +27,85 @@
         </option>
       </b-select>
     </b-field>
+    <b-table
+      focusable
+      hoverable
+      narrowed
+      detailed
+      style="width:90%;margin-left:5%;margin-right:5%"
+      default-sort="name"
+      :data="sharedOutList"
+      :selected.sync="selected"
+      :current-page.sync="currentPage"
+      :paginated="true"
+      :per-page="perPage"
+      :pagination-simple="true"
+      :default-sort-direction="defaultSortDirection"
+      @dblclick="(row) => $router.push(getConAddr(row))"
+      @keyup.native.enter="$router.push(getConAddr(selected))"
+      @keyup.native.space="$router.push(getConAddr(selected))"
+    >
+      <template slot-scope="props">
+        <b-table-column
+          sortable
+          field="name"
+          :label="$t('message.table.name')"
+        >
+          <span clss="has-text-weight-bold">
+            <b-icon
+              icon="folder"
+              size="is-small"
+            />
+            {{ props.row }}
+          </span>
+        </b-table-column>
+      </template>
+      <template 
+        slot="detail"
+        slot-scope="props"
+      >
+        <SharedDetails
+          :container="props.row"
+        />
+      </template>
+      <template slot="empty">
+        <p
+          style="text-align:center;margin-top:5%;margin-bottom:5%"
+        >
+          {{ $t('message.emptyShared') }}
+        </p>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
+import SharedDetails from "@/components/SharedDetails";
+
 export default {
+  components: {
+    SharedDetails
+  },
   data: function () {
     return {
-      sharedList: [],
+      sharedOutList: [],
       selected: undefined,
       perPage: 15,
       defaultSortDirection: "asc",
       currentPage: 1,
     }
   },
+  beforeMount () {
+    this.getSharedContainers();
+  },
+  methods: {
+    getSharedContainers: function () {
+      this.$store.state.client.get_share(
+        this.$route.params.user
+      ).then(
+        (ret) => {this.sharedOutList = ret;}
+      );
+    },
+  }
 }
 </script>
