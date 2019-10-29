92584492cb17e317ed3172530746c5aec041447e
sampsapenna <sampsa.penna@csc.fi>
2019-10-29 09:38:48 +0200

    Add deploy script files.

---
+++ deploy/app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+THE_HOST=${HOST:="0.0.0.0"}
+THE_PORT=${PORT:="9090"}
+
+echo "Start swift-x-account-sharing backend"
+
+gunicorn swift_x_account_sharing.server:init_server --bind $THE_HOST:$THE_PORT --worker-class aiohttp.GunicornUVLoopWebWorker --workers 1
