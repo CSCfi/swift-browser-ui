3d7c5a05cabab0ef7e667f217991bc2ab80f647b
sampsapenna <sampsa.penna@csc.fi>
2019-11-12 17:59:43 +0200

    Reduce gunicorn workers to 1 because database problems.

--- deploy/app.sh
+++ deploy/app.sh
@@ -8,5 +8,4 @@ echo "Start swift-sharing-request backend"
 gunicorn swift_sharing_request.server:init_server \
     --bind $THE_HOST:$THE_PORT \
     --worker-class aiohttp.GunicornUVLoopWebWorker \
-    --workers 4 \
-    --timeout 30
+    --workers 1 \
