8de1f841a3d86c363f3dd184a68ec1334adf75b0
sampsapenna <sampsa.penna@csc.fi>
2019-12-17 12:32:14 +0200

    Use setuptools package discovery

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         "test": ["tox", "pytest", "pytest-cov", "coverage", "flake8",
                  "flake8-docstrings", "asynctest"],
     },
-    packages=[__name__],
+    packages=setuptools.find_packages(),
     platforms="any",
     entry_points={
         "console_scripts": [
