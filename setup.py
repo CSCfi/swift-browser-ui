a066509fdba84aaa2dc6b2a6835a074f906863a6
sampsapenna <sampsa.penna@csc.fi>
2020-02-26 16:13:04 +0200

    Make package installable

---
+++ setup.py
@@ -0,0 +1,51 @@
+import setuptools
+from swift_upload_runner import __name__, __version__, __author__
+
+
+setuptools.setup(
+    name=__name__,
+    version=__version__,
+    description='Swift upload / download runner for swift-browser-ui',
+    author=__author__,
+    author_email="sampsa.penna@csc.fi",
+    project_urls={
+        'Source': 'https://github.com/CSCfi/swift-upload-runner',
+    },
+    license='MIT',
+    install_requires=[
+        'aiohttp',
+        'python-swiftclient',
+        'keystoneauth1',
+        'gunicorn',
+        'uvloop',
+        "swift-browser-ui"
+        "@ git+https://github.com/cscfi/swift-browser-ui.git@0.7.1"
+    ],
+    extras_require={
+        'test': ['tox', 'pytest', 'pytest-cov', 'coverage',
+                 'tox', 'flake8', 'flake8-docstrings', 'pytest-aiohttp',
+                 'asynctest'],
+    },
+    packages=[__name__],
+    include_package_data=True,
+    platforms='any',
+    entry_points={
+        'console_scripts': [
+            'swift-upload-runner=swift_browser_ui.server:run_server',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
