d6499e87c67efd533accc19464e3a76039826d74
--global <sampsa.penna@csc.fi>
2020-03-17 12:44:58 +0200

    Revise folder upload to use resumable.js

--- swift_browser_ui_frontend/src/components/FolderUpload.vue
+++ swift_browser_ui_frontend/src/components/FolderUpload.vue
@@ -1,77 +1,42 @@
 <template>
-  <form 
-    enctype="multipart/form-data"
-    method="post"
-    name="contFolders"
-    @submit.prevent="callUpload ()"
-  >
-    <input
-      id="directoryFileList"
-      type="file"
-      name="fileList"
-      webkitdirectory
-      multiple
-    >
-    <input
-      type="submit"
-      value="Upload a folder"
-    >
-  </form>
+  <span :id="id" />
 </template>
 
 <script>
-import {
-  getUploadSignature,
-  swiftCreateContainer,
-} from "@/common/api";
-
 export default {
   name: "FolderUploadForm",
-  methods: {
-    callUpload: function () {
-      // Wrapper for the asynchronous upload function
-      this.folderUpload().then(() => {
-        console.log("Called folder upload");
-      });
+  props: {
+    dropelement: {
+      default: "",
+      type: String,
     },
-    folderUpload: async function () {
-      // Upload a folder using Openstack Swift FormPost API
-
-      let containerName = "swift-browser-ui-upload-".concat(
-        Date.now().toString()
-      );
-      console.log(containerName);
-      let form = document.forms.namedItem("contFolders");
-      let files = form.elements["fileList"];
-      console.log(files);
-      let uploadData = new FormData(form);
-
-      console.log("Creating container");
-      await swiftCreateContainer(containerName);
-      console.log("Container creation successful");
-
-      let signature = await getUploadSignature(
-        containerName,
-        files.files.length,
-        undefined
-      );
-      console.log(signature);
-
-      uploadData.append("redirect", "");
-      uploadData.append("max_file_size", signature.max_file_size);
-      uploadData.append("max_file_count", signature.max_file_count);
-      uploadData.append("expires", signature.expires);
-      uploadData.append("signature", signature.signature);
-
-      let address = new URL(signature.host.concat(signature.path));
-
-      console.log(uploadData);
-
-      await fetch(address, {
-        method: "POST",
-        body: uploadData,
-      });
-      console.log("File upload successful");
+  },
+  data: function () {
+    return {
+      id: "upload-".concat(Date.now().toString()),
+      containerName: undefined,
+      address: "",
+      resum: undefined,
+    };
+  },
+  computed: {
+    active () {
+      return this.$store.state.active;
+    },
+    res () {
+      return this.$store.state.resumableClient;
+    },
+  },
+  mounted () {
+    this.registerToResumable();
+  },
+  methods: {
+    registerToResumable: function () {
+      // Add elements to listen
+      this.res.assignBrowse(document.getElementById(this.id));
+      if(this.dropelement != "") {
+        this.res.assignDrop(document.getElementById(this.dropelement));
+      }
     },
   },
 };
