1d6db19aebbbd21303079d82f3a4525d95a879c6
--global <sampsa.penna@csc.fi>
2020-03-17 12:31:42 +0200

    Add upload progress bar

---
+++ swift_browser_ui_frontend/src/components/UploadProgressBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-progress
+    v-if="isChunking && !isUploading"
+    type="is-primary"
+    size="is-large"
+  >
+    Chunking
+  </b-progress>
+  <b-progress
+    v-else-if="isUploading"
+    type="is-primary"
+    :value="progress"
+    size="is-large"
+    show-value
+  >
+    Uploading {{ (progress * 100).toFixed(1) }}%
+  </b-progress>
+</template>
+
+<script>
+export default {
+  name: "ProgressBar",
+  computed: {
+    isChunking () {
+      return this.$store.state.isChunking;
+    },
+    isUploading () {
+      return this.$store.state.isUploading;
+    },
+    progress () {
+      return this.$store.state.uploadProgress;
+    },
+  },
+};
+</script>
