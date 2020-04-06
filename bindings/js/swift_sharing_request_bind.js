cfd5be6e0e0b66ab1e721408790664ef060b6c7c
sampsapenna <sampsa.penna@csc.fi>
2020-04-06 12:40:08 +0300

    Fix linter errors

--- bindings/js/swift_sharing_request_bind.js
+++ bindings/js/swift_sharing_request_bind.js
@@ -5,7 +5,7 @@ class SwiftSharingRequest {
   
   constructor (
     address,
-    signatureAddress = ""
+    signatureAddress = "",
   ) {
     this.address = address;
     this.signatureAddress = signatureAddress;
@@ -13,7 +13,7 @@ class SwiftSharingRequest {
 
   async _getSignature(
     validFor,
-    toSign
+    toSign,
   ) {
     // Get a signature for an API call.
     if (this.signatureAddress != "") {
@@ -21,7 +21,7 @@ class SwiftSharingRequest {
         this.signatureAddress);
       signatureUrl.searchParams.append("path", toSign);
       let signed = await fetch(
-        signatureUrl, {method: "GET", credentials: "same-origin"}
+        signatureUrl, {method: "GET", credentials: "same-origin"},
       );
       return signed.json();
     }
@@ -33,7 +33,7 @@ class SwiftSharingRequest {
   async addAccessRequest(
     username,
     container,
-    owner
+    owner,
   ) {
     // Add a request for container access.
     let url = new URL("/request/user/".concat(username, "/", container),
@@ -42,79 +42,79 @@ class SwiftSharingRequest {
 
     let signed = await this._getSignature(
       60,
-      "/request/user/".concat(username, "/", container)
+      "/request/user/".concat(username, "/", container),
     );
 
     url.searchParams.append("valid", signed.valid_until);
     url.searchParams.append("signature", signed.signature);
 
     let resp = fetch(
-      url, {method: "POST"}
+      url, {method: "POST"},
     ).then(
-      (resp) => {return resp.json();}
+      (resp) => {return resp.json();},
     );
     return resp;
   }
 
   async listMadeRequests(
-    username
+    username,
   ) {
     let url = new URL("/request/user/".concat(username),
       this.address);
 
-      let signed = await this._getSignature(
-        60,
-        "/request/user/".concat(username)
-      );
-      url.searchParams.append("valid", signed.valid_until);
-      url.searchParams.append("signature", signed.signature);
+    let signed = await this._getSignature(
+      60,
+      "/request/user/".concat(username),
+    );
+    url.searchParams.append("valid", signed.valid_until);
+    url.searchParams.append("signature", signed.signature);
   
-      let resp = fetch(
-      url, {method: "GET"}
+    let resp = fetch(
+      url, {method: "GET"},
     ).then(
-      (resp) => {return resp.json();}
+      (resp) => {return resp.json();},
     );
     return resp;
   }
 
   async listOwnedRequests(
-    username
+    username,
   ) {
     let url = new URL("/request/owner/".concat(username),
       this.address);
 
-      let signed = await this._getSignature(
-        60,
-        "/request/owner/".concat(username)
-      );
-      url.searchParams.append("valid", signed.valid_until);
-      url.searchParams.append("signature", signed.signature);
+    let signed = await this._getSignature(
+      60,
+      "/request/owner/".concat(username),
+    );
+    url.searchParams.append("valid", signed.valid_until);
+    url.searchParams.append("signature", signed.signature);
   
-      let resp = fetch(
-      url, {method: "GET"}
+    let resp = fetch(
+      url, {method: "GET"},
     ).then(
-      (resp) => {return resp.json();}
+      (resp) => {return resp.json();},
     );
     return resp;
   }
 
   async listContainerRequests(
-    container
+    container,
   ) {
     let url = new URL("/request/container/".concat(container),
       this.address);
 
-      let signed = await this._getSignature(
-        60,
-        "/request/container/".concat(container)
-      );
-      url.searchParams.append("valid", signed.valid_until);
-      url.searchParams.append("signature", signed.signature);
+    let signed = await this._getSignature(
+      60,
+      "/request/container/".concat(container),
+    );
+    url.searchParams.append("valid", signed.valid_until);
+    url.searchParams.append("signature", signed.signature);
   
-      let resp = fetch(
-      url, {method: "GET"}
+    let resp = fetch(
+      url, {method: "GET"},
     ).then(
-      (resp) => {return resp.json();}
+      (resp) => {return resp.json();},
     );
     return resp;
   }
@@ -122,28 +122,28 @@ class SwiftSharingRequest {
   async shareDeleteAccess(
     username,
     container,
-    owner
+    owner,
   ) {
     // Delete the details of an existing share action.
     let url = new URL(
-      "/request/user/".concat(username, "/", container), this.address
+      "/request/user/".concat(username, "/", container), this.address,
     );
     url.searchParams.append("owner", owner);
 
     let signed = await this._getSignature(
       60,
-      "/request/user/".concat(username, "/", container)
+      "/request/user/".concat(username, "/", container),
     );
     url.searchParams.append("valid", signed.valid_until);
     url.searchParams.append("signature", signed.signature);
 
     let deleted = fetch(
-      url, {method: "DELETE"}
+      url, {method: "DELETE"},
     ).then(
       (resp) => {
         if (resp.status == 200) {return true;}
         else {return false;}
-      }
+      },
     );
     return deleted;
   }    
