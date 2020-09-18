465bc697a0f73f072799d7d480505427ccfe8a32
Stefan Negru <stefan@blankdots.com>
2020-09-18 13:16:37 +0300

    bump to 1.0.0rc5

--- swift_browser_ui/__init__.py
+++ swift_browser_ui/__init__.py
@@ -6,7 +6,7 @@ with the object storage.
 """
 
 
-__name__ = 'swift_browser_ui'
-__version__ = '1.0.0rc5'
-__author__ = 'CSC Developers'
-__license__ = 'MIT License'
+__name__ = "swift_browser_ui"
+__version__ = "1.0.0rc6"
+__author__ = "CSC Developers"
+__license__ = "MIT License"
