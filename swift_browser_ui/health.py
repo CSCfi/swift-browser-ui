b9d6586b2180f7709b222361355a7a69540e774e
Stefan Negru <stefan@blankdots.com>
2020-11-17 18:51:58 +0200

    catch error getting info in health

--- swift_browser_ui/health.py
+++ swift_browser_ui/health.py
@@ -5,6 +5,7 @@ import typing
 import time
 
 import aiohttp.web
+from aiohttp.client_exceptions import ServerDisconnectedError
 
 from .settings import setd
 from .signature import sign
@@ -18,26 +19,32 @@ async def get_x_account_sharing(
         performance: typing.Dict[str, typing.Any]
 ) -> None:
     """Poll swift-x-account-sharing API."""
-    if setd["sharing_internal_endpoint"]:
-        start = time.time()
-        async with web_client.get(
-                str(setd["sharing_internal_endpoint"]) + "/health",
-                params=api_params
-        ) as resp:
-            request.app["Log"].debug(resp)
-            if resp.status != 200:
-                services["swift-x-account-sharing"] = {
-                    "status": "Down",
-                }
-            else:
-                sharing_status = await resp.json()
-                services["swift-x-account-sharing"] = sharing_status
-        performance["swift-x-account-sharing"] = {
-            "time": time.time() - start
-        }
-    else:
+    try:
+        if setd["sharing_internal_endpoint"]:
+            start = time.time()
+            async with web_client.get(
+                    str(setd["sharing_internal_endpoint"]) + "/health",
+                    params=api_params
+            ) as resp:
+                request.app["Log"].debug(resp)
+                if resp.status != 200:
+                    services["swift-x-account-sharing"] = {
+                        "status": "Down",
+                    }
+                else:
+                    sharing_status = await resp.json()
+                    services["swift-x-account-sharing"] = sharing_status
+            performance["swift-x-account-sharing"] = {
+                "time": time.time() - start
+            }
+        else:
+            services["swift-x-account-sharing"] = {
+                "status": "Nonexistent"
+            }
+    except ServerDisconnectedError:
+        request.app["Log"].debug("Poll swift-x-account-sharing failed")
         services["swift-x-account-sharing"] = {
-            "status": "Nonexistent"
+            "status": "Error"
         }
 
 
@@ -49,26 +56,32 @@ async def get_swift_sharing(
         performance: typing.Dict[str, typing.Any]
 ) -> None:
     """Poll swift-sharing-request API."""
-    if setd["request_internal_endpoint"]:
-        start = time.time()
-        async with web_client.get(
-                str(setd["request_internal_endpoint"]) + "/health",
-                params=api_params
-        ) as resp:
-            request.app["Log"].debug(resp)
-            if resp.status != 200:
-                services["swift-sharing-request"] = {
-                    "status": "Down",
-                }
-            else:
-                request_status = await resp.json()
-                services["swift-sharing-request"] = request_status
-        performance["swift-sharing-request"] = {
-            "time": time.time() - start
-        }
-    else:
+    try:
+        if setd["request_internal_endpoint"]:
+            start = time.time()
+            async with web_client.get(
+                    str(setd["request_internal_endpoint"]) + "/health",
+                    params=api_params
+            ) as resp:
+                request.app["Log"].debug(resp)
+                if resp.status != 200:
+                    services["swift-sharing-request"] = {
+                        "status": "Down",
+                    }
+                else:
+                    request_status = await resp.json()
+                    services["swift-sharing-request"] = request_status
+            performance["swift-sharing-request"] = {
+                "time": time.time() - start
+            }
+        else:
+            services["swift-sharing-request"] = {
+                "status": "Nonexistent"
+            }
+    except ServerDisconnectedError:
+        request.app["Log"].debug("Poll swift-x-account-sharing failed")
         services["swift-sharing-request"] = {
-            "status": "Nonexistent"
+            "status": "Error"
         }
 
 
@@ -80,26 +93,32 @@ async def get_upload_runner(
         performance: typing.Dict[str, typing.Any]
 ) -> None:
     """Poll swiftui-upload-runner API."""
-    if setd["upload_internal_endpoint"]:
-        start = time.time()
-        async with web_client.get(
-                str(setd["upload_internal_endpoint"]) + "/health",
-                params=api_params
-        ) as resp:
-            request.app["Log"].debug(resp)
-            if resp.status != 200:
-                services["swiftui-upload-runner"] = {
-                    "status": "Down",
-                }
-            else:
-                upload_status = await resp.json()
-                services["swiftui-upload-runner"] = upload_status
-        performance["swiftui-upload-runner"] = {
-            "time": time.time() - start
-        }
-    else:
+    try:
+        if setd["upload_internal_endpoint"]:
+            start = time.time()
+            async with web_client.get(
+                    str(setd["upload_internal_endpoint"]) + "/health",
+                    params=api_params
+            ) as resp:
+                request.app["Log"].debug(resp)
+                if resp.status != 200:
+                    services["swiftui-upload-runner"] = {
+                        "status": "Down",
+                    }
+                else:
+                    upload_status = await resp.json()
+                    services["swiftui-upload-runner"] = upload_status
+            performance["swiftui-upload-runner"] = {
+                "time": time.time() - start
+            }
+        else:
+            services["swiftui-upload-runner"] = {
+                "status": "Nonexistent"
+            }
+    except ServerDisconnectedError:
+        request.app["Log"].debug("Poll swift-x-account-sharing failed")
         services["swiftui-upload-runner"] = {
-            "status": "Nonexistent"
+            "status": "Error"
         }
 
 
@@ -147,7 +166,7 @@ async def handle_health_check(
     status["performance"] = performance
 
     for service in services.values():
-        if service["status"] == "Down":
+        if service["status"] in ["Down", "Error"]:
             status["status"] = "Partially down"
             break
         if service["status"] == "Degraded":
