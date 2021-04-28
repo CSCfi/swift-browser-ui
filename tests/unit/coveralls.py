6e0d5acc555f63744be72d816cfb6a2115aa712d
Sampsa penna <24293337+sampsapenna@users.noreply.github.com>
2021-04-28 15:37:02 +0300

    Update tests/unit/coveralls.py
    Co-authored-by: Stefan Negru <blankdots@users.noreply.github.com>

--- tests/unit/coveralls.py
+++ tests/unit/coveralls.py
@@ -11,7 +11,7 @@ import sys
 if __name__ == "__main__":
     if "COVERALLS_REPO_TOKEN" in os.environ:
         rc = call("coveralls")
-        sys.stdout.write("Coveralls report from TRAVIS CI.\n")
+        sys.stdout.write("Coveralls report from CI.\n")
         # raise SystemExit(rc)
     else:
         sys.stdout.write("Not on TRAVIS CI.\n")
