c11e40d0bc8f473ef61f781a0c174946c889d04d
Sampsa Penna <sampsa.penna@csc.fi>
2021-05-07 13:32:24 +0300

    change index from modal

--- swift_browser_ui_frontend/src/pages/Index.vue
+++ swift_browser_ui_frontend/src/pages/Index.vue
@@ -3,117 +3,118 @@
 <!-- things into components, but that would break the language support and -->
 <!-- make index page development more difficult. -->
 <template>
-  <div class="hero is-fullheight">
-    <div class="hero-body">
-      <div class="container">
-        <div class="columns is-centered">
-          <div
-            id="login-center"
-            class="column is-4-desktop is-3-widescreen box"
+  <div class="indexpage">
+    <div class="contents">
+      <div>
+        <a 
+          href="#"
+          class="center"
+        >
+          <img
+            src="@/assets/csc_logo.svg"
+            :alt="$t('message.cscOrg')"
           >
-            <div class="columns is-centered">
-              <a href="#">
-                <img
-                  src="@/assets/csc_logo.svg"
-                  :alt="$t('message.cscOrg')"
-                >
-              </a>
-            </div>
-            <div>
-              <b-message
-                v-if="unauth"
-                :title="$t('message.error.Unauthorized')"
-                type="is-warning"
-                has-icon
-              >
-                {{ $t('message.error.Unauthorized_text') }}
-              </b-message>
-              <b-message
-                v-if="forbid"
-                :title="$t('message.error.Forbidden')"
-                type="is-danger"
-                has-icon
-              >
-                {{ $t('message.error.Forbidden_text') }}
-              </b-message>
-              <b-message
-                v-if="notfound"
-                :title="$t('message.error.Notfound')"
-                type="is-warning"
-                has-icon
-              >
-                {{ $t('message.error.Notfound_text') }}
-              </b-message>
-              <b-message
-                v-if="uidown"
-                :title="$t('message.error.UIdown')"
-                type="is-warning"
-                has-icon
-              >
-                {{ $t('message.error.UIdown_text') }}
-              </b-message>
-            </div>
-            <div
-              v-if="!forbid"
-              class="buttons"
-            >
-              <a
-                class="button is-primary is-fullwidth"
-                href="/login"
-              >{{ $t("message.index.logIn") }}</a>
-            </div>
-            <div
-              v-if="notindex"
-              class="buttons"
-            >
-              <a
-                class="button is-primary is-fullwidth"
-                href="/"
-              >{{ $t("message.error.frontPage") }}</a>
-            </div>
-            <b-field class="locale-changer">
-              <b-select
-                v-model="$i18n.locale"
-                placeholder="Language"
-                icon="earth"
-                expanded
-                @input="setCookieLang ()"
-              >
-                <option
-                  v-for="lang in langs"
-                  :key="lang.value"
-                  :value="lang.value"
-                >
-                  {{ lang.ph }}
-                </option>
-              </b-select>
-            </b-field>
-            <footer class="footer">
-              <div class="content has-text-centered">
-                <p>
-                  <strong>{{ $t("message.program_name") }}</strong>
-                  {{ $t("message.devel") }}
-                  <a
-                    href="https://csc.fi"
-                    :alt="$t('message.cscOrg')"
-                  >{{ $t("message.cscOrg") }}</a>
-                </p>
-              </div>
-            </footer>
-          </div>
-        </div>
+        </a>
+      </div>
+      <div>
+        <h2 class="is-csc-secondary">{{ $t("message.program_name") }}</h2>
+        <p>{{ $t("message.program_description") }}</p>
+      </div>
+      <div>
+        <b-message
+          v-if="unauth"
+          :title="$t('message.error.Unauthorized')"
+          type="is-warning"
+          has-icon
+        >
+          {{ $t('message.error.Unauthorized_text') }}
+        </b-message>
+        <b-message
+          v-if="forbid"
+          :title="$t('message.error.Forbidden')"
+          type="is-danger"
+          has-icon
+        >
+          {{ $t('message.error.Forbidden_text') }}
+        </b-message>
+        <b-message
+          v-if="notfound"
+          :title="$t('message.error.Notfound')"
+          type="is-warning"
+          has-icon
+        >
+          {{ $t('message.error.Notfound_text') }}
+        </b-message>
+        <b-message
+          v-if="uidown"
+          :title="$t('message.error.UIdown')"
+          type="is-warning"
+          has-icon
+        >
+          {{ $t('message.error.UIdown_text') }}
+        </b-message>
+      </div>
+      <div
+        v-if="!forbid"
+        class="buttons"
+      >
+        <a
+          class="button is-primary center"
+          href="/login"
+        >{{ $t("message.index.logIn") }}</a>
+      </div>
+      <div
+        v-if="notindex"
+        class="buttons"
+      >
+        <a
+          class="button is-primary center"
+          href="/"
+        >{{ $t("message.error.frontPage") }}</a>
+      </div>
+      <b-field class="locale-changer center">
+        <b-select
+          v-model="$i18n.locale"
+          placeholder="Language"
+          icon="earth"
+          expanded
+          @input="setCookieLang ()"
+        >
+          <option
+            v-for="lang in langs"
+            :key="lang.value"
+            :value="lang.value"
+          >
+            {{ lang.ph }}
+          </option>
+        </b-select>
+      </b-field>
+      <div class="content has-text-centered">
+        <p>
+          {{ $t("message.devel") }}
+          <a
+            href="https://csc.fi"
+            :alt="$t('message.cscOrg')"
+          >{{ $t("message.cscOrg") }}</a>
+        </p>              
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.hero-body #login-center{
-    padding: 30px 20px 20px 20px;
+.indexpage {
+  width: 40%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
 }
-
-.hero-body .footer {
-    margin:15px 0;
-    padding:0;
+.contents {
+  flex: 1 0 auto;
+}
+.center {
+  width: 50%;
+  margin: auto;
 }
 </style>
