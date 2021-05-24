74dbbd3e3c25814aee55e503355e8b3329a3630a
Sampsa Penna <sampsa.penna@csc.fi>
2021-05-24 12:08:42 +0300

    revert to defaults for language

--- swift_browser_ui_frontend/config/lang_overrides.js
+++ swift_browser_ui_frontend/config/lang_overrides.js
@@ -1,24 +1,4 @@
 // Parameterized text override example file
-let lang_overrides = {
-  en: {
-    message: {
-      program_name: "SD Connect",
-      program_description: "SD Connect provides a simple-to-use web user " +
-        "interface, along with a command line interface, for storing " +
-        "encrypted sensitive data for the duration of your research project " +
-        "by allowing data uploads through drag-n-drop, and simple data " +
-        "sharing.",
-    },
-  },
-  fi: {
-    message: {
-      program_name: "SD Connect",
-      program_description: "SD Connect tarjoaa yksinkertaisen " +
-        "web-käyttöliittymän ja komentorivikäyttöliittymän sensitiivisen " +
-        "datan säilyttämiseen tutkimusprojektin ajaksi, mahdollistamalla " +
-        "tiedostojen lähettämisen raahaamalla ja yksinkertaisen jakamisen.",
-    },
-  },
-};
+let lang_overrides = {};
 
 export default lang_overrides;
