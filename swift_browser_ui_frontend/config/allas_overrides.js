65c3a93403df51bc3f44db06d1d483c026971a48
Sampsa Penna <sampsa.penna@csc.fi>
2021-05-28 15:10:03 +0300

    add allas branding build files

---
+++ swift_browser_ui_frontend/config/allas_overrides.js
@@ -0,0 +1,24 @@
+// Parameterized text override example file
+let lang_overrides = {
+  en: {
+    message: {
+      program_name: "Allas Web UI",
+      program_description: "A web UI for browsing Allas object storage. " +
+        "The UI allows uploading files in bulk, including files over 5GiB " +
+        "and provides functionality for simple sharing of containers " +
+        "across projects.",
+    },
+  },
+  fi: {
+    message: {
+      program_name: "Allas Web UI",
+      program_description: "Web-käyttöliittymä Allas " +
+        "objektitietojärjestelmään. Käyttölittymä mahdollistaa tiedostojen " +
+        "lähettämisen massana mukaanlukien yli 5gt tiedostot, ja " +
+        "mahdollistaa säiliöiden jakamisen yksinkertaisesti projektien " +
+        "välillä.",
+    },
+  },
+};
+
+export default lang_overrides;
