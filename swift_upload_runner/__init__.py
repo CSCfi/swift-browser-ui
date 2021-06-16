59770e6b0b4be88c444cd72c69c537e6a1174f72
Sampsa Penna <sampsa.penna@csc.fi>
2021-06-16 17:05:49 +0300

    bump to v0.2.0 due to resumable upload deprecation

--- swift_upload_runner/__init__.py
+++ swift_upload_runner/__init__.py
@@ -1,6 +1,6 @@
 """Runner for swift-browser-ui upload and replication operations."""
 
 __name__ = "swift_upload_runner"
-__version__ = "0.1.8"
+__version__ = "0.2.0"
 __author__ = "CSC Developers"
 __license__ = "MIT License"
