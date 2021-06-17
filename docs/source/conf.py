7698cfc4681c70c70532d12ff74f68f4fb71671f
Sampsa Penna <sampsa.penna@csc.fi>
2021-06-17 11:14:49 +0300

    bump to v1.1.0b2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = f"{current_year}, CSC Developers"
 author = "CSC Developers"
 
 # The full version, including alpha/beta/rc tags
-version = release = "1.1.0b1"
+version = release = "1.1.0b2"
 
 
 # -- General configuration ---------------------------------------------------
