b6e40440787ed3612666bf4aab404d5948e95236
Stefan Negru <stefan@blankdots.com>
2021-08-12 16:17:43 +0300

    add cypress config

---
+++ tests/cypress/support/commands.js
@@ -0,0 +1,37 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+const baseUrl = "http://localhost:" + Cypress.env("port") + "/"
+
+Cypress.Commands.add("login", ( loginMessage ) => {
+    cy.visit(baseUrl)
+    cy.contains(loginMessage).parent().click()
+    cy.wait(1000)
+    cy.get('#inputbox').type('abcdefabcdefabcdefabcdefabcdefab')
+    cy.get('#retform > [type="submit"]').click()
+    cy.wait(1000)
+  })
+  
