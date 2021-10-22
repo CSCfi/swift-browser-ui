5903c8bda7ad3adf10aac6be4cdbb09ce7921d1f
Sampsa Penna <sampsa.penna@csc.fi>
2021-10-22 08:56:13 +0300

    add skeleton structure for encrypted uploads

--- swift_browser_ui_frontend/src/common/router.js
+++ swift_browser_ui_frontend/src/common/router.js
@@ -14,6 +14,7 @@ import ReplicationView from "@/views/Replicate";
 import TokensView from "@/views/Tokens";
 import CreateContainer from "@/views/AddContainer";
 import DirectShare from "@/views/DirectShare";
+import EncryptedUpload from "@/views/EncryptedUpload";
 
 Vue.use(Router);
 
@@ -59,6 +60,11 @@ export default new Router({
       path: "/browse/:user/:project/sharing/sharedirect",
       name: "DirectShare",
       component: DirectShare,
+    },
+    {
+      path: "/browse/encrypt/:project/:container",
+      name: "EncryptedUpload",
+      component: EncryptedUpload,
     },
     {
       path: "/browse/:user/:project/:container/shared/:owner",
---
+++ swift_browser_ui_frontend/src/views/EncryptedUpload.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    id="encryption"
+    class="contents"
+  >
+    <b-field label="Private Key">
+      <b-input
+        v-model="privkey"
+        placeholder="Sender private key"
+        type="textarea"
+        maxlength="1024"
+      />
+    </b-field>
+    <b-field label="Public Key">
+      <b-input
+        v-model="pubkey"
+        placeholder="Sender public key"
+        type="textarea"
+        maxlength="1024"
+      />
+    </b-field>
+    <b-field label="Receiver Public Keys">
+      <b-taginput
+        v-model="recvkeys"
+        placeholder="Paste a receiver public key"
+        type="textarea"
+      />
+    </b-field>
+    <b-field label="Container">
+      <b-input
+        v-model="container"
+        placeholder="Upload container"
+      />
+    </b-field>
+    <b-field>
+      <b-upload
+        v-model="dropFiles"
+        multiple
+        drag-drop
+        expanded
+      >
+        <section class="section">
+          <div class="content has-text-centered">
+            <p>
+              <b-icon
+                icon="upload"
+                size="is-large"
+              />
+            </p>
+            <p>Drop files that will be encrypted and uploaded.</p>
+          </div>
+        </section>
+      </b-upload>
+    </b-field>
+    <b-button
+      type="is-success"
+      icon-left="upload"
+      @click="encryptAndUpload"
+    >
+      Encrypt and Upload
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EncryptedUpload",
+  data() {
+    return {
+      privkey: "",
+      pubkey: "",
+      recvkeys: [],
+      container: "",
+      dropFiles: [],
+    };
+  },
+  methods: {
+    encryptAndUpload: function () {
+      this.$buefy.toast.open("Init upload");
+    },
+  },
+};
+</script>
+
+<style scoped>
+#encryption {
+  width: 80%;
+  margin: auto;
+}
+</style>
