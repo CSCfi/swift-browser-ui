656babd1332ba21c9931e7174e6a198b7e7d11ad
Sampsa Penna <sampsa.penna@csc.fi>
2022-06-15 14:55:05 +0300

    correctly find new languageSelector elements

--- tests/cypress/integration/userInfo.cy.js
+++ tests/cypress/integration/userInfo.cy.js
@@ -11,8 +11,9 @@ describe("Login and log out a user", function () {
     })
 
     it("should login user with Finnish to username + password and remember the selection", () => {
-        cy.get("In English").click()
-        cy.get("Suomeksi").click()
+        cy.visit(Cypress.config().baseUrl)
+        cy.contains("In English").click({ position: "top" })
+        cy.contains("Suomeksi").click({ position: "top" })
         cy.login(' Kirjaudu SSO:ta käyttäen ')
         cy.get('.buttons > .router-link-exact-active').should(($browse) => {
             expect($browse).to.have.length(1)
