f80da8936d144524fe41480bbe76872c1a9a4d06
Sampsa Penna <sampsa.penna@csc.fi>
2022-06-21 10:36:31 +0300

    delay event handler initialization until completion

--- swift_browser_ui_frontend/wasm/js/crypt-post.js
+++ swift_browser_ui_frontend/wasm/js/crypt-post.js
@@ -1,6 +1,6 @@
 // Normal required listeners for starting the service worker
 self.addEventListener("install", (event) => {
-  event.waitUntil(new Promise(waitAsm));  // defined in crypt-pre.js
+  event.waitUntil(self.skipWaiting());
 });
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
@@ -8,147 +8,153 @@ self.addEventListener("activate", (event) => {
 
 let sessionPtr, fileName, headerPtr, headerLen, chunkPtr, chunkLen, chunk, header, chunkArray;
 
-self.addEventListener("message", (e) => {
-  switch(e.data.cmd) {
-    case "initFileSystem":
-      FS.mkdir("/keys");
-      FS.mkdir("/keys/recv_keys");
-      FS.mkdir("/data");
-      // Create output device
-      e.source.postMessage({
-        eventType: "wasmFilesystemInitialized",
-      });
-      break;
-    case "appendPubkey":
-      FS.writeFile(
-        "/keys/recv_keys/pubkey_" + e.data.keyname.toString(),
-        e.data.pubkey,
-      );
-      e.source.postMessage({
-        eventType: "wasmFilesystemPubkeyAdded",
-        publicKey: e.data.pubkey,
-        publicKeyOrder: e.data.keyname,
-      });
-      break;
-    case "addPrivKey":
-      FS.writeFile(
-        "/keys/pk.key",
-        e.data.privkey,
-      );
-      e.source.postMessage({
-        eventType: "wasmFilesystemPrivkeyAdded",
-        privKey: e.data.privkey,
-      })
-      break;
-    case "initEphemeral":
-      sessionPtr = Module.ccall(
-        "open_session_eph",
-        "number",
-        [],
-        [],
-      );
-      fileName = e.data.filename + ".c4gh";
-      e.source.postMessage({
-        eventType: "encryptSessionInitiated",
-        ptr: sessionPtr,
-      });
-      break;
-    case "initNormal":
-      sessionPtr = Module.ccall(
-        "open_session",
-        "number",
-        ["string"],
-        [e.data.passphrase],
-      );
-      fileName = e.data.filename + ".c4gh";
-      e.source.postMessage({
-        eventType: "encryptSessionInitiated",
-        ptr: sessionPtr,
-      });
-      break;
-    case "createHeader":
-      header = Module.ccall(
-        "wrap_crypt4gh_header",
-        "number",
-        ["number"],
-        [sessionPtr],
-      );
-      headerPtr = Module.ccall(
-        "wrap_chunk_content",
-        "number",
-        ["number"],
-        [header]
-      );
-      headerLen = Module.ccall(
-        "wrap_chunk_len",
-        "number",
-        ["number"],
-        [header],
-      )
-      // Copy chunk buffer contents to new array and answer
-      e.source.postMessage({
-        eventType: "encryptedHeaderReady",
-        header: new Uint8Array(HEAPU8.subarray(headerPtr, headerPtr + headerLen)),
-      });
-      // Free header memory
-      Module.ccall(
-        "free_chunk",
-        "number",
-        ["number"],
-        [header],
-      );
-      break;
-    case "encryptChunk":
-      chunkArray = new Uint8Array(e.data.chunk);
-      chunk = Module.ccall(
-        "encrypt_chunk",
-        "number",
-        ["number", "array", "number"],
-        [sessionPtr, chunkArray, chunkArray.length],
-      );
-      chunkPtr = Module.ccall(
-        "wrap_chunk_content",
-        "number",
-        ["number"],
-        [header]
-      );
-      chunkLen = Module.ccall(
-        "wrap_chunk_len",
-        "number",
-        ["number"],
-        [header],
-      );
-      // Respond with the chunk, and ask for the next one
-      e.source.postMessage({
-        eventType: "nextChunk",
-        chunk: new Uint8Array(HEAPU8.subarray(chunkPtr, chunkPtr + chunkLen)),
-        iter: e.data.iter,
-      });
-      Module.ccall(
-        "free_chunk",
-        "number",
-        ["number"],
-        [chunk],
-      )
-      break;
-    case "cleanUp":
-      try {
-        Module.ccall(
-          "clean_session",
-          undefined,
+console.log(Module);
+console.log(FS);
+
+wasmReady.then(() => {
+  console.log("Adding sw event listeners.");
+  self.addEventListener("message", (e) => {
+    switch(e.data.cmd) {
+      case "initFileSystem":
+        FS.mkdir("/keys");
+        FS.mkdir("/keys/recv_keys");
+        FS.mkdir("/data");
+        // Create output device
+        e.source.postMessage({
+          eventType: "wasmFilesystemInitialized",
+        });
+        break;
+      case "appendPubkey":
+        FS.writeFile(
+          "/keys/recv_keys/pubkey_" + e.data.keyname.toString(),
+          e.data.pubkey,
+        );
+        e.source.postMessage({
+          eventType: "wasmFilesystemPubkeyAdded",
+          publicKey: e.data.pubkey,
+          publicKeyOrder: e.data.keyname,
+        });
+        break;
+      case "addPrivKey":
+        FS.writeFile(
+          "/keys/pk.key",
+          e.data.privkey,
+        );
+        e.source.postMessage({
+          eventType: "wasmFilesystemPrivkeyAdded",
+          privKey: e.data.privkey,
+        })
+        break;
+      case "initEphemeral":
+        sessionPtr = Module.ccall(
+          "open_session_eph",
+          "number",
+          [],
+          [],
+        );
+        fileName = e.data.filename + ".c4gh";
+        e.source.postMessage({
+          eventType: "encryptSessionInitiated",
+          ptr: sessionPtr,
+        });
+        break;
+      case "initNormal":
+        sessionPtr = Module.ccall(
+          "open_session",
+          "number",
+          ["string"],
+          [e.data.passphrase],
+        );
+        fileName = e.data.filename + ".c4gh";
+        e.source.postMessage({
+          eventType: "encryptSessionInitiated",
+          ptr: sessionPtr,
+        });
+        break;
+      case "createHeader":
+        header = Module.ccall(
+          "wrap_crypt4gh_header",
+          "number",
           ["number"],
           [sessionPtr],
         );
-        FS.rmdir("/keys/recv_keys")
-        FS.rmdir("/keys");
-        FS.rmdir("/data");
-      } catch (e) {
-        console.log("FS ignoring error on remove ", e);
-      } finally {
+        headerPtr = Module.ccall(
+          "wrap_chunk_content",
+          "number",
+          ["number"],
+          [header]
+        );
+        headerLen = Module.ccall(
+          "wrap_chunk_len",
+          "number",
+          ["number"],
+          [header],
+        )
+        // Copy chunk buffer contents to new array and answer
         e.source.postMessage({
-          eventType: "cleanUpDone",
+          eventType: "encryptedHeaderReady",
+          header: new Uint8Array(HEAPU8.subarray(headerPtr, headerPtr + headerLen)),
         });
-      }
-      break;
-  }
+        // Free header memory
+        Module.ccall(
+          "free_chunk",
+          "number",
+          ["number"],
+          [header],
+        );
+        break;
+      case "encryptChunk":
+        chunkArray = new Uint8Array(e.data.chunk);
+        chunk = Module.ccall(
+          "encrypt_chunk",
+          "number",
+          ["number", "array", "number"],
+          [sessionPtr, chunkArray, chunkArray.length],
+        );
+        chunkPtr = Module.ccall(
+          "wrap_chunk_content",
+          "number",
+          ["number"],
+          [header]
+        );
+        chunkLen = Module.ccall(
+          "wrap_chunk_len",
+          "number",
+          ["number"],
+          [header],
+        );
+        // Respond with the chunk, and ask for the next one
+        e.source.postMessage({
+          eventType: "nextChunk",
+          chunk: new Uint8Array(HEAPU8.subarray(chunkPtr, chunkPtr + chunkLen)),
+          iter: e.data.iter,
+        });
+        Module.ccall(
+          "free_chunk",
+          "number",
+          ["number"],
+          [chunk],
+        )
+        break;
+      case "cleanUp":
+        try {
+          Module.ccall(
+            "clean_session",
+            undefined,
+            ["number"],
+            [sessionPtr],
+          );
+          FS.rmdir("/keys/recv_keys")
+          FS.rmdir("/keys");
+          FS.rmdir("/data");
+        } catch (e) {
+          console.log("FS ignoring error on remove ", e);
+        } finally {
+          e.source.postMessage({
+            eventType: "cleanUpDone",
+          });
+        }
+        break;
+    }
+  });
 });
--- swift_browser_ui_frontend/wasm/js/crypt-pre.js
+++ swift_browser_ui_frontend/wasm/js/crypt-pre.js
@@ -6,11 +6,18 @@ var Module = {
   },
 };
 
-var waitAsm = (resolve, reject) => {
-  if (moduleStartComplete) {
-    resolve(true);
-    return;
-  } else {
-    setTimeout(waitAsm, 250, resolve, reject);
-  }
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+async function waitAsm() {
+  console.log("Begin wait for WASM readiness.");
+  while (!moduleStartComplete) {
+    console.log("Waiting 250ms for WASM readiness.");
+    await timeout(250);
+  }
+  console.log("Module start finalized. Ready to answer.");
+  return true;
+}
+
+var wasmReady = waitAsm();
