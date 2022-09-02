ccb0ae972fe42207f130b3d8b804b61ded108244
Sauli Purhonen <sauli.purhonen@visma.com>
2022-09-02 11:09:06 +0300

    Get shared containers

--- swift_browser_ui_frontend/src/components/ContainerTable.vue
+++ swift_browser_ui_frontend/src/components/ContainerTable.vue
@@ -126,6 +126,8 @@ export default {
       const sharingContainers = await this.getSharingContainers();
       const sharedContainers = await this.getSharedContainers();
 
+      sharedContainers && console.log("sharedConts: ", sharedContainers);
+
       const getSharingStatus = (folderName) => {
         if (sharingContainers.indexOf(folderName) > -1) {
           return this.$t("message.share.sharing_status");
