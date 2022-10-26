47601c12056c57d3ba8d17876737ce741140bdeb
Sampsa Penna <sampsa.penna@csc.fi>
2022-10-26 10:52:33 +0300

    add patch to common handler unit tests

--- tests/common/test_common_handlers.py
+++ tests/common/test_common_handlers.py
@@ -14,7 +14,7 @@ class PreflightHandlerTestCase(unittest.IsolatedAsyncioTestCase):
         """Test handle_delete_preflight handler function."""
         resp = await swift_browser_ui.common.common_handlers.handle_delete_preflight(None)
         self.assertEqual(
-            resp.headers["Access-Control-Allow-Methods"], "POST, OPTIONS, DELETE"
+            resp.headers["Access-Control-Allow-Methods"], "POST, OPTIONS, DELETE, PATCH"
         )
         self.assertEqual(resp.headers["Access-Control-Max-age"], "84600")
         self.assertIsInstance(resp, aiohttp.web.Response)
