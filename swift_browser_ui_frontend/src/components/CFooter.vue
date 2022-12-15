92be1c02030b43c7352ffc5523b2b315bdf7ec3c
rquazi <rumana.quazi@csc.fi>
2022-12-15 14:13:46 +0200

    fixing code styling

--- swift_browser_ui_frontend/src/components/CFooter.vue
+++ swift_browser_ui_frontend/src/components/CFooter.vue
@@ -1,36 +1,34 @@
 <template>
   <div id="c-footer">
     <nav class="navbar">
-    <div class="navbar-menu is-active">
+      <div class="navbar-menu is-active">
         <div class="navbar-start" >
           <div class="col-3">
             <div class="row navbar-item " >
               <span class="largetext">
-              {{ $t("message.footerMenu.groupandprogram") }}
-            </span>
+                {{ $t("message.footerMenu.groupandprogram") }}
+              </span>
             </div>
             <div class="row navbar-item smalltext">
-              <a class="linktext"
-              href="https://csc.fi"
-              :alt="$t('message.footerMenu.csc')"
-            >{{
-              $t("message.footerMenu.csc")
-            }}</a>
+              <a class="linktext" href="https://csc.fi"
+                :alt="$t('message.footerMenu.csc')"
+              >
+                {{ $t("message.footerMenu.csc") }}
+              </a>
             </div>
           </div>        
         </div>
     
         <div class="navbar-end" id="rightmenu"> 
-            <div class="navbar-item smalltext">
-                <a class="linktext"
-              href="https://research.csc.fi/-/sd-connect"
-              :alt="$t('message.footerMenu.servicedescription')"
-            >{{
-              $t("message.footerMenu.servicedescription")
-            }}</a>
+          <div class="navbar-item smalltext">
+              <a class="linktext" href="https://research.csc.fi/-/sd-connect"
+                :alt="$t('message.footerMenu.servicedescription')"
+                >
+                  {{ $t("message.footerMenu.servicedescription") }}
+              </a>
           </div> 
           <div class="navbar-item smalltext">
-            <p>{{ $t("message.footerMenu.accessibility") }}</p> 
+              <p>{{ $t("message.footerMenu.accessibility") }}</p> 
           </div>
         </div>
       </div>
@@ -49,40 +47,38 @@ export default {
 <style lang="scss" scoped>
 @import "@/css/prod.scss";
 
-
 #c-footer{
   height:  fit-content;
   width: 100%;
   padding:40px 60px;
-  background-color:#DFE1E3;
+  background-color:$csc-lightest-grey;
   z-index: 2;
 }
 
 .navbar{
-  background-color:#DFE1E3;
+  background-color:$csc-lightest-grey;
   padding: auto;
   width: 100%;  
  
 }
 .navbar-menu{
-  background-color:#DFE1E3; 
+  background-color:$csc-lightest-grey; 
   width: 100%;
   height: 100%;
   box-shadow: none;
 }
 
 .navbar-start{
-  background-color:#DFE1E3; ;
+  background-color:$csc-lightest-grey;
 }
 
 .navbar-end{
-  background-color:#DFE1E3; ;
+  background-color:$csc-lightest-grey;
 }
 
 #leftmenu{
   padding-left: 10px;
 }
-
 
 .largetext{
   font-family: var(--csc-font-family);
@@ -90,7 +86,7 @@ export default {
   font-weight: 600;
   font-size: 16px;
   line-height: 19px;
-  color: #595959;
+  color: $csc-grey;
 }
 .smalltext{
   font-family:  var(--csc-font-family);;
@@ -98,16 +94,14 @@ export default {
   font-weight: 400;
   font-size: 14px;
   line-height: 18px;
-  color: #595959;
+  color: $csc-grey;
 }
 .linktext {
-  color: #595959; 
+  color: $csc-grey; 
   font-weight: 400;
 }
 a:hover {
   background-color: #CCF4F0;
   text-decoration: underline;     
 }
-
-
 </style>
