180ef73e6c3ab186680d407cae3fccd700900bae
Emmi Rehn <emmi.rehn@csc.fi>
2023-02-24 12:13:57 +0200

    Create folder modal is not so small now

--- swift_browser_ui_frontend/src/components/CreateFolderModal.vue
+++ swift_browser_ui_frontend/src/components/CreateFolderModal.vue
@@ -218,11 +218,24 @@ export default {
   max-height: 75vh;
 }
 
-@media screen and (max-height: 720px), (max-width: 992px ) {
-  .add-folder {
-    max-height: 50vh;
+@media screen and (max-width: 773px), (max-height: 580px) {
+   .add-folder {
+    top: -5rem;
   }
 }
+
+@media screen and (max-height: 580px) and (max-width: 773px), 
+(max-width: 533px) {
+  .add-folder {
+    top: -9rem;
+  }
+}
+
+@media screen and (max-height: 580px) and (max-width: 533px) {
+  .add-folder {
+    top: -13rem;
+   }
+ }
 
 c-card-content {
   color: var(--csc-dark-grey);
