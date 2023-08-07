fe52737c287a2dd04a97826c4382c691e3ebd0d1
Ville Muilu <vmuilu@csc.fi>
2023-08-07 09:48:07 +0300

    moved error handling to nav guards

--- swift_browser_ui_frontend/src/pages/IndexPage.vue
+++ swift_browser_ui_frontend/src/pages/IndexPage.vue
@@ -139,12 +139,6 @@ export default {
   mounted: function () {
     checkIDB().then(result => this.idb = result);
   },
-  methods: {
-    getParams: function(){
-      const url = new URL(window.location.href);
-      return url.searchParams.get("error");
-    },
-  },
 };
 </script>
 
