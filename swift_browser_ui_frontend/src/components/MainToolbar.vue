c283008e4d587e9ea8136ee74634595df48638e7
HangLe <lhang@csc.fi>
2024-02-02 12:53:04 +0200

    Add link to Accessibility page's toolbar to route back to front page

---
+++ swift_browser_ui_frontend/src/components/MainToolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <c-toolbar class="relative">
+    <c-link
+      href="/"
+      target="_self"
+      color="dark-grey"
+      weight="400"
+    >
+      <c-csc-logo />
+      <span>{{ $t('message.program_name') }} </span>
+    </c-link>
+    <c-spacer />
+    <LanguageSelector />
+  </c-toolbar>
+</template>
+
+<script>
+import LanguageSelector from "@/components/CLanguageSelector.vue";
+export default {
+  components:{
+    LanguageSelector,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+c-toolbar span {
+  display: inline-flex;
+  align-items: center;
+  margin-left: 1rem;
+}
+</style>
