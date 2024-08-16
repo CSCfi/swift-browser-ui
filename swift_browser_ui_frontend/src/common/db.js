cc45321c50809b92b2ea088f9b0920f5f43d30d5
Monika Radaviciute <mradavic@csc.fi>
2024-08-16 13:05:05 +0300

    Upgrade dexie and disable liveQuery cache

--- swift_browser_ui_frontend/src/common/db.js
+++ swift_browser_ui_frontend/src/common/db.js
@@ -2,22 +2,21 @@ import Dexie from "dexie";
 import { DEV } from "@/common/conv";
 
 function initDB() {
-  const db = new Dexie("sd-connect");
+  const db = new Dexie("sd-connect", { cache: "disabled" });
   db.version(1).stores({
     projects: "&id, name",
     containers: "++id, &[projectID+name], *tags, *tokens",
     objects: "++id, &[containerID+name], *tags, *tokens",
     preferences: "id",
   });
-  db.preferences.count(count => {
+  db.preferences.count((count) => {
     if (count === 0) {
-      db.preferences.add({id: 1});
+      db.preferences.add({ id: 1 });
     }
   });
 
   return db;
 }
-
 
 // From https://github.com/dexie/Dexie.js/issues/613#issuecomment-841608979
 let DB;
