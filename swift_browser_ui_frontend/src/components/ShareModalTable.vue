817cf413dd070210a21872c09bd3904dc6cce082
Monika Radaviciute <mradavic@csc.fi>
2026-04-17 13:28:30 +0300

    Fix share permissions not displayed correctly in table

--- swift_browser_ui_frontend/src/components/ShareModalTable.vue
+++ swift_browser_ui_frontend/src/components/ShareModalTable.vue
@@ -218,6 +218,7 @@ export default {
                     fontSize: "0.875rem",
                     marginBottom: "-1.5rem",
                   },
+                  returnObject: true,
                   items: this.accessRights,
                   value: item.access.length > 0
                     ? (
@@ -237,21 +238,6 @@ export default {
                     } else {
                       this.clearPermChange();
                     }
-                  },
-                  onClick: ({ event }) => {
-                    const wrapper =
-                      document.getElementById("share-card-modal-content");
-                    let wrapperPosition = wrapper.getBoundingClientRect();
-                    let targetPosition = event.target.getBoundingClientRect();
-                    let diff = wrapperPosition.bottom - targetPosition.bottom;
-                    const ul = event.target.shadowRoot.
-                      activeElement.parentNode.nextSibling.querySelector("ul");
-                    setTimeout(() => {
-                      const ulPosition = ul.getBoundingClientRect();
-                      if (diff < ulPosition.height) {
-                        wrapper.scrollBy(0, ulPosition.height - diff);
-                      }
-                    }, 150);
                   },
                 },
               },
