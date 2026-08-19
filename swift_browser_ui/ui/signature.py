4991456ebebf7b679e08bbc76c435ea166c067b7
Sampsa Penna <sampsa.penna@csc.fi>
2026-08-19 17:52:26 +0300

    prevent forging request with a user session for invalid projects

--- swift_browser_ui/ui/signature.py
+++ swift_browser_ui/ui/signature.py
@@ -28,9 +28,32 @@ async def handle_signature_request(
             reason="Signable path missing from query string."
         )
 
-    return aiohttp.web.json_response(
-        await swift_browser_ui.ui._convenience.sign(valid_for, path_to_sign)
-    )
+    # Validate that the signed path is accessing content allowed in context
+    # by checking the signed path contains one of the allowed projects
+    matching_project = False
+    for project in session["projects"].keys():
+        if project in path_to_sign:
+            matching_project = True
+
+        # Also check against the project name
+        if session["projects"][project]["name"] in path_to_sign:
+            matching_project = True
+
+    # Allow ids to be queried for any project
+    if path_to_sign.startswith("/ids") and request.method == "GET":
+        matching_project = True
+
+    if matching_project:
+        return aiohttp.web.json_response(
+            await swift_browser_ui.ui._convenience.sign(valid_for, path_to_sign)
+        )
+    else:
+        # Raise in case user tries accessing project beyond verified scope
+        LOGGER.info(f"Could not find a valid project in {path_to_sign}")
+        LOGGER.info(f"Valid projects were {list(session['projects'].keys())}")
+        raise aiohttp.web.HTTPForbidden(
+            reason="Path contains a project not accessible in login scope."
+        )
 
 
 async def handle_ext_token_create(request: aiohttp.web.Request) -> aiohttp.web.Response:
