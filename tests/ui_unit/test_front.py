6f9b6f0cdd5c5dc162aa270ba2451eec66015af4
Sampsa Penna <sampsa.penna@csc.fi>
2026-09-22 11:28:36 +0300

    fix frontend tests breaking due to incorrectly formatted session

--- tests/ui_unit/test_front.py
+++ tests/ui_unit/test_front.py
@@ -34,7 +34,7 @@ class FrontendTestCase(
         session_return["at"] = time.time()
         session_return["token"] = "placeholder"
         session_return["uname"] = "placeholder"
-        session_return["projects"] = "placeholder"
+        session_return["projects"] = {"test-project-1": {"token": "example-token"}}
         aiohttp_session_get_session_mock = unittest.mock.AsyncMock()
         aiohttp_session_get_session_mock.return_value = session_return
         p_get_sess = unittest.mock.patch(
@@ -59,7 +59,7 @@ class FrontendTestCase(
         )
         session_return["at"] = time.time()
         session_return["token"] = "placeholder"
-        session_return["projects"] = "placeholder"
+        session_return["projects"] = {"test-project-1": {"token": "example-token"}}
         aiohttp_session_get_session_mock = unittest.mock.AsyncMock()
         aiohttp_session_get_session_mock.return_value = session_return
         p_get_sess = unittest.mock.patch(
@@ -88,7 +88,7 @@ class FrontendTestCase(
         )
         session_return["at"] = time.time()
         session_return["token"] = "placeholder"
-        session_return["projects"] = "placeholder"
+        session_return["projects"] = {"test-project-1": {"token": "example-token"}}
         aiohttp_session_get_session_mock = unittest.mock.AsyncMock()
         aiohttp_session_get_session_mock.return_value = session_return
         p_get_sess = unittest.mock.patch(
